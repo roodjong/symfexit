6641535a12e102730886aeb0e8fe0b3077a54405
ssuverkropp <sander.suverkropp@proton.me>
2025-10-11 14:26:39 +0000

    Add WellKnownPermissionGroup to refer to standard groups

--- symfexit/adminsite/management/commands/create_permission_groups.py
+++ symfexit/adminsite/management/commands/create_permission_groups.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Permission
 from django.core.management import BaseCommand
+
+from symfexit.adminsite.models import WellKnownPermissionGroup, get_or_create
 
 
 class Command(BaseCommand):
     help = "Creates and updates default permission groups"
 
     def handle(self, *args, **options):
-        view_all_group, created = Group.objects.get_or_create(name="View all")
+        view_all_group_ref = get_or_create(code=WellKnownPermissionGroup.WellKnownPermissionGroups.VIEW_ALL)
+        view_all_group = view_all_group_ref.group
         all_view_permissions = Permission.objects.filter(codename__startswith="view")
         view_all_group.permissions.set(all_view_permissions)
--- symfexit/adminsite/models.py
+++ symfexit/adminsite/models.py
@@ -1,1 +1,37 @@
-# Create your models here.
+from django.contrib.auth.models import Group
+from django.db import IntegrityError, models
+
+
+def get_or_create(code):
+    try:
+        group = WellKnownPermissionGroup.objects.get(code=code.value)
+    except WellKnownPermissionGroup.DoesNotExist:
+        name = code.label
+        group = None
+        while group is None:
+            try:
+                group = Group.objects.create(name=name)
+            except IntegrityError:
+                name = name + " (well known)"
+        well_known = WellKnownPermissionGroup(code=code.value, group=group)
+        well_known.save()
+    return group
+
+class WellKnownPermissionGroup(models.Model):
+    class WellKnownPermissionGroups(models.TextChoices):
+        VIEW_ALL = "view_all", "View all"
+
+    code = models.CharField(
+        unique=True,
+        max_length=255,
+        choices=WellKnownPermissionGroups.choices,
+    )
+
+    group = models.OneToOneField(
+        Group,
+        on_delete=models.PROTECT,
+        related_name="well_known_code",
+    )
+
+    def __str__(self):
+        return self.code
