ff67a7974b9154611ed7614f7ff7dd2eb897fb46
RanaFlumine <99541098+RanaFlumine@users.noreply.github.com>
2025-10-18 17:46:55 +0200

    fix lint

--- symfexit/adminsite/admin.py
+++ symfexit/adminsite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.models import Group
+
 from .models import GroupFlags
 
 admin.site.unregister(Group)
