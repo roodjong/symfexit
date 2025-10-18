175322284f4ce3e579b61a8223f4bb389a452e76
RanaFlumine <99541098+RanaFlumine@users.noreply.github.com>
2025-10-18 19:23:12 +0200

    Remove the mass action to delete members. (#210)
    * Remove the mass action to delete members.
    Fixes: #209
    
    * okauy i fixed it only for members now

--- symfexit/members/admin.py
+++ symfexit/members/admin.py
@@ -325,6 +325,13 @@ class UserAdmin(admin.ModelAdmin):
             return False
         return super().has_delete_permission(request, obj)
 
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        # Remove the "delete_selected" mass action for Members only
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
+
     def has_change_permission(self, request, obj: User = None):
         if obj is None:
             return super().has_change_permission(request, obj)
