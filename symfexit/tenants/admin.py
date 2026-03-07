3d2ef5937a7a73c4fe2ae3d65ba97bffae615993
Jelle Besseling <jelle@pingiun.com>
2026-03-07 18:23:24 +0100

    Seperate management site

--- symfexit/tenants/admin.py
+++ symfexit/tenants/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django_tenants.admin import TenantAdminMixin
 
-from symfexit.tenants.models import Client
+from symfexit.tenants.adminsite import global_admin
+from symfexit.tenants.models import Client, Domain
 
 
-@admin.register(Client)
+class DomainInline(admin.TabularInline):
+    model = Domain
+    extra = 0
+
+
+@admin.register(Client, site=global_admin)
 class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
     list_display = ("name",)
+    inlines = (DomainInline,)
