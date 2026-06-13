4b9add10177daa466a095c64ced7c453d12737c2
Jelle <ik-jelle@hotmail.nl>
2026-06-13 23:54:40 +0200

    feat(export): added to one relationships abilty to export
    Added support for foreignkeys and OneToOne relationsships, defaults to null if relationship is missing

--- symfexit/root/export/types.py
+++ symfexit/root/export/types.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
 
 type field_with_label = tuple[str, str]
-type fields = Sequence[str | field_with_label]
+type field_with_model = tuple[str, fields]
+type fields = Sequence[str | field_with_label | field_with_model]
