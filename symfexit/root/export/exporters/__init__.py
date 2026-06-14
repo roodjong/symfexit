01b8b9f690475cd828a39926118b26e43218362c
Jelle <ik-jelle@hotmail.nl>
2026-06-14 15:17:50 +0200

    feat(export): added excel exporter

--- symfexit/root/export/exporters/__init__.py
+++ symfexit/root/export/exporters/__init__.py
@@ -1,6 +1,8 @@
 from symfexit.root.export.exporters._abstract_exporter import AbstractExporter
+from symfexit.root.export.exporters.excel_exporter import ExcelExporter
 from symfexit.root.export.exporters.json_exporter import JsonExporter
 
 exporters: dict[str, AbstractExporter] = {
+    "Excel": ExcelExporter(),
     "JSON": JsonExporter(),
 }
